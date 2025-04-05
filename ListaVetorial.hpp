b756af282cad0b2c06b5a02ab747c61c0754d860
juuubs_bs <julianasimon@edu.univali.br>
2025-04-05 10:32:32 -0300

    primeiro commit

---
+++ ListaVetorial.hpp
@@ -0,0 +1,10 @@
+#ifndef TAM_MAX
+#define TAM_MAX 50
+#endif
+
+template <typename T>
+struct listaVet{
+    
+
+
+};
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "ListaVetorial.hpp"
+#include "LUE.hpp"
+
+int main(){
+
+
+
+    
+
+
+
+
+
+
+
+    return 0;
+}
