48655b9ab2a17e0c846f8fa3062e5fd60d63251c
juuubs_bs <julianasimon@edu.univali.br>
2025-04-07 17:01:51 -0300

    peq

---
+++ LUE.hpp
@@ -0,0 +1,165 @@
+template<typename T>
+struct No{
+    T dado;
+    No *elo; // nó seguinte a ele
+}; 
+
+template<typename T>
+struct LUE{
+    No<T> *comeco;
+    No<T> *fim;
+    No<T> *ant;    //posição a ser escolhida
+    No<T> *prox;   //posição a ser escolhida
+    No<T> *aux;    //posição a ser escolhida
+    int contador;
+
+    void instanciar(){
+        comeco = nullptr;
+        fim = nullptr;
+        contador = 0;
+    }
+
+    void inserir(T valor){
+        No<T> *novoNo = new No<T>;
+        novoNo->dado = valor;
+        novoNo->elo = nullptr;
+
+        //caso seja o primeiro da lista
+        if(comeco == nullptr){
+            comeco = novoNo;
+            fim = novoNo;
+        }
+        //caso seja o menor de todos
+        else if(valor < comeco->dado){
+            novoNo->elo = comeco;//atualiza o endereço
+            comeco = novoNo;//repassa os dados
+
+        }
+        //caso seja o maior de todos
+        else if(valor > fim->dado){
+            fim->elo = novoNo; 
+            fim = novoNo;
+        }
+        //caso seja no meio
+        else{
+            ant = comeco; 
+            prox = ant->elo;
+
+            while(prox != NULL){
+                if(ant->dado < valor && valor < prox->dado){
+                    ant->elo = novoNo;
+                    novoNo->elo = prox;
+                    break;
+                }
+                ant = prox; 
+                prox = ant->elo;
+            }
+        }
+        contador++;
+    }
+
+    void imprimir(){
+        this->aux = this->comeco;
+
+        while(this->aux != NULL){
+            std:: cout << this->aux->dado << " ";
+            this->aux = this->aux->elo;
+        }
+
+    }
+
+    bool retirar(T valor){
+        //achar o valor
+        No <T> *aux1 = comeco;
+        while(aux1 != NULL){ // passa pela lista
+            if(aux1->dado == valor){ // tenta achar o valor
+                if(aux1 == comeco){
+                    // unico valor na lista
+                    if(aux1 == fim){ 
+                        comeco = NULL;
+                        fim = NULL;
+                    }
+                    //retirar no começo
+                    else {
+                        comeco = aux1->elo;
+                    }
+                    delete aux1;
+                    contador--;
+                }
+                //retirar no final
+                else if(aux1 == fim){
+                    fim = ant;
+                    fim->elo = NULL;
+                    delete aux1;
+                    contador--;
+                }
+                //retirar no meio
+                else{
+                    ant->elo = prox;
+                    delete aux1;
+                    contador--;
+                }
+                return true;
+            }
+            ant = aux1;         //avança uma casa
+            aux1 = aux1->elo;
+            prox = aux1->elo;
+        }
+        return false;
+    }
+    
+    bool contemItem(T valor){
+
+        No<T> *aux1 = comeco;
+
+        while(aux1 != NULL){
+            if(aux1->dado == valor){
+                return true;
+            }
+            else{
+                aux1 = aux1->elo;
+            }
+        }
+        return false;
+
+    }
+
+    T obterItem(int posicao){
+
+        No<T> *aux1 = comeco;
+        int i = 0;
+
+        while(aux1 != NULL){
+            //regrede uma no main
+            if(i == posicao){
+                std:: cout << aux1->dado;
+                return aux1->dado;
+            }
+            else{
+                aux1 = aux1->elo;
+                i++;
+            }
+        } 
+
+        return (T)NULL;
+    }
+
+    int descobrirIndice(T valor){
+
+        No<T> *aux1 = comeco;
+        int i = 0;
+
+        while( aux1 != NULL){
+            if(aux1->dado == valor){
+                std:: cout << "o i:" << i;
+                std:: cout << "o valor:" << aux1->dado;
+                return i;
+            }
+            else{
+                i++;
+                aux1 = aux1->elo;
+            }
+        }
+        return -1;
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,32 +1,35 @@
 #include <iostream>
-#include "ListaVetorial2.cpp"
+//#include "ListaVetorial2.cpp"
 #include "LUE.hpp"
 
+struct Peca{
+    int cima;
+    int baixo;
+};
+
+struct Jogador{
+
+};
+
+void IniciaListaDePeca(LUE<Peca> & l){
+
+}
+
+
 int main(){
-    
-    Lista<Peca> pecas;
-    Lista<Peca> jogador1;
-    Lista<Peca> jogador2;
-    //Lista<Peca> jogador3;
-    //Lista<Peca> jogador4;
+    LUE<int> pecas;
+    //LUE<Jogador> jogadores;
 
+    pecas.instanciar();
 
-    pecas.inicializar(2);
+    pecas.inserir(8);
+    pecas.inserir(3);
+    pecas.inserir(1);
+    pecas.inserir(7);
 
-    pecas.mostrarPecas();
+    pecas.descobrirIndice(10);
 
-    jogador1.receber(pecas);
-    //pecas.distribuir(jogador3);
-    //pecas.distribuir(jogador4);
-
-    jogador1.mostrarPecas();
-    std:: cout << "agora vem as pecas dnv: " << std:: endl;
-    pecas.mostrarPecas();
-
-    //funcionandinho
-
-
-
+    pecas.imprimir();
 
 
 
